5c6d47d5cca84bc0b1bc0e46ace8f09607fb2e4b
Vinicius <viniciusflexa@gmail.com>
2025-02-05 18:32:39 -0300

    style: change logout button to color destructive

--- app/_components/navUser.tsx
+++ app/_components/navUser.tsx
@@ -113,7 +113,7 @@ export function NavUser({
                             </DropdownMenuItem>
                         </DropdownMenuGroup>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem onClick={signOut}>
+                        <DropdownMenuItem onClick={signOut} className="text-destructive">
                             <LogOut />
                             Sair
                         </DropdownMenuItem>
