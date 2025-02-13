4fc6ba5163536c2030a1cfbe2c749098c85ada7a
Vinicius <viniciusflexa@gmail.com>
2025-02-13 18:52:20 -0300

    feat: add user id

--- app/products/_components/productsTable.tsx
+++ app/products/_components/productsTable.tsx
@@ -1,9 +1,10 @@
 import { columns, Product as ProductType } from "@/app/_components/_columns/products";
 import { DataTableProducts } from "@/app/_components/dataTableProducts";
 import { getProductsWithOrders } from "@/app/_actions/product";
+import { User } from "@/app/_types/user";
 
-export const ProductsTable = async () => {
-    const products = await getProductsWithOrders();
+export const ProductsTable = async ({ id }: User) => {
+    const products = await getProductsWithOrders(id);
 
     const data: ProductType[] = products.map((product) => ({
         ...product,
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,17 @@
 import { ToggleDashboardTable } from "../_components/toggleDashboardTable";
 import { ProductsTable } from "./_components/productsTable";
+import { getCurrentUser } from "@/app/_utils/authServer";
+import { redirect } from "next/navigation";
 
-export default function Products() {
+export default async function Products() {
+    const userId = await getCurrentUser();
+
+    if (!userId) redirect("/login");
+
     return (
         <ToggleDashboardTable
             dashboard={<p>Dashboard</p>}
-            table={<ProductsTable />}
+            table={<ProductsTable id={userId} />}
         />
     );
 }
